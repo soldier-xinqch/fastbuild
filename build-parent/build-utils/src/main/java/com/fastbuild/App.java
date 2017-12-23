60bebf1108491323d188c63cd26d1ad09361cfa0
xinqch <xinchao32119@163.com>
2017-12-23 15:03:03 +0800

    快速搭建平台

---
+++ build-parent/build-utils/src/main/java/com/fastbuild/App.java
@@ -0,0 +1,13 @@
+package com.fastbuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
